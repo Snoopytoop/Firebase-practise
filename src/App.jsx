8258d47d4595e31b5680d16deac0b1536f4d1b92
Snoopytoop <samuelluke1998@gmail.com>
2023-06-15 12:34:17 +0100

    finished tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import React from "react";
 import "./App.css";
 import { auth } from "./firebase/Init";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({})
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => { 
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false)
+      console.log(user)
+      if (user) {
+        setUser(user)
+      }
+    })
+  }, [])
+
   function register() {
-    console.log("hi");
-    createUserWithEmailAndPassword(auth, "email@mail.com", "password")
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "john@smith.com", "password123")
       .then((user) => {
         console.log(user);
       })
@@ -16,21 +32,30 @@ function App() {
         console.log(error);
       });
   }
-
   function login() {
-    signInWithEmailAndPassword(auth, "email@mail.com", "passwordddd")
-      .then((user) => {
+    console.log("login");
+    signInWithEmailAndPassword(auth, "john@smith.com", "password123")
+      .then(({user}) => {
         console.log(user);
+        setUser(user)
       })
       .catch((error) => {
-        console.log(error.message); 
+        console.log(error.message);
       });
+  }
+  function logout() {
+    signOut(auth)
+    setUser({})
   }
 
   return (
     <div className="App">
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      <div className="text">
+      {loading ? "loading..." : user.email}
+      </div>
     </div>
   );
 }
